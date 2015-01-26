64004454c02f0845819662e63555e71a310835fc
Chris Hartman <chris.m.hartman@gmail.com>
2015-01-26 12:56:14 -0900

    Initial commit from local git after creating project

---
+++ CS372Spring2015Examples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS372Spring2015Examples
+//
+//  Created by Chris Hartman on 1/26/15.
+//  Copyright (c) 2015 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
